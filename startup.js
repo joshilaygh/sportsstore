f0965c130098c4b4b0cfd09408a2a887eb422b3c
Dhiraj Joshi <joshjoshilay@gmail.com>
2025-03-10 17:56:28 +1300

    Add SSL config

--- startup.js
+++ startup.js
@@ -20,6 +20,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 // Middleware
