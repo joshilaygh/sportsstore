a2f50e667cd9c01d4ab959dfa80709620d7c0ef8
Dhiraj Joshi <joshjoshilay@gmail.com>
2025-03-10 17:51:47 +1300

    Port changed to 8080 and changes in deployment upload version

--- startup.js
+++ startup.js
@@ -386,8 +386,5 @@ app.get("/my-basket", async (req, res) => {
 
 
 
-
-
-
 // Start the server
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(8080, () => console.log("Server running on port 8080"));
